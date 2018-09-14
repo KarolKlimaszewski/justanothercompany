196c602087875a1214358554ace966a5a7118241
Karol Klimaszewski <karol.klimaszewski1997@gmail.com>
2018-09-14 15:43:53 +0200

    added remaining part of slider

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,28 @@ $(function () {
         submenu.hide();
     })
 
+    let leftButton = $('.slider__prev');
+    let rightButton = $('.slider__next');
+    let allSlides = $('.slider-list__item');
+    let counter = 0;
+
+    rightButton.on("click", function (e) {
+        e.preventDefault();
+        console.log(allSlides);
+        allSlides.eq(counter).toggleClass("slider-list__item--visible");
+        if(counter < allSlides.length - 1 ? counter++ : counter = 0);
+        allSlides.eq(counter).toggleClass("slider-list__item--visible");
+    })
+    leftButton.on("click", function (e) {
+        e.preventDefault();
+        console.log(allSlides);
+        allSlides.eq(counter).toggleClass("slider-list__item--visible");
+        if(counter === 0 ? counter = allSlides.length - 1 : counter--)
+        allSlides.eq(counter).toggleClass("slider-list__item--visible");
+    })
+
+
+
 
 
 
